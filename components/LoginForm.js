7b16199e02872ef79a52f855170cef25d267fb00
henshammi <henshammi@gmail.com>
2024-11-30 17:34:44 +0700

    push fix login

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
+import { FaEye } from "react-icons/fa6";
+import { FaEyeSlash } from "react-icons/fa6";
+import "../styles/Login.css";
+import Head from "next/head";
+import Container from "@components/Container";
+
+const styles = {
+  error: {
+    color: "red",
+    textAlign: "center",
+    marginBottom: "10px",
+  },
+};
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
 
   const saveTokenAndRedirect = (token) => {
     localStorage.setItem("authToken", token); // Simpan token ke localStorage
@@ -45,86 +59,105 @@ const LoginForm = () => {
   };
 
   return (
-    <div style={styles.container}>
-      <h2 style={styles.title}>Login</h2>
+    <>
+      <Head>
+        <title>Login &mdash; EpicNews</title>
+      </Head>
+      <Container>
+        <div className="login-main">
+          <div className="login-left hidden-custom">
+            <img src={"/image_login.png"} alt="" />
+          </div>
+          <div className="login-right">
+            <div className="login-right-container">
+              <div className="login-logo">
+                <img src={"/logo.png"} alt="logo epicnews" />
+              </div>
+              <div className="login-center">
+                <h2>Welcome back!</h2>
+                <p>Please enter your details</p>
 
-      {error && <p style={styles.error}>{error}</p>}
+                <form onSubmit={handleLogin}>
+                  <input
+                    type="text"
+                    placeholder="Username or Email"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
 
-      <form onSubmit={handleLogin} style={styles.form}>
-        <input
-          type="text"
-          placeholder="Username or Email"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          style={styles.input}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={styles.input}
-          required
-        />
-        <button type="submit" style={styles.button}>
-          Login
-        </button>
-      </form>
+                  <div className="pass-input-div">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                    {showPassword ? (
+                      <FaEyeSlash
+                        onClick={() => {
+                          setShowPassword(!showPassword);
+                        }}
+                      />
+                    ) : (
+                      <FaEye
+                        onClick={() => {
+                          setShowPassword(!showPassword);
+                        }}
+                      />
+                    )}
+                  </div>
 
-      <button onClick={handleGithubLogin} style={styles.githubButton}>
-        Login with GitHub
-      </button>
-    </div>
+                  {error && <p style={styles.error}>{error}</p>}
+
+                  <div className="login-center-options">
+                    <a href="#" className="forgot-pass-link">
+                      Forgot password?
+                    </a>
+                  </div>
+
+                  <div className="login-center-buttons">
+                    <button type="submit">Log In</button>
+
+                    <button type="button" onClick={handleGithubLogin}>
+                      <img src={"/icons8-github.svg"} alt="" />
+                      Log In with GitHub
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <p className="login-bottom-p">
+                Don't have an account? <a href="/register">Sign Up</a>
+              </p>
+            </div>
+          </div>
+          <div className="relative">
+            <a href="/" className="absolute top-10 right-14">
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-x"
+                  className="contrast-50"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18"></line>
+                  <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+              </button>
+            </a>
+          </div>
+        </div>
+      </Container>
+    </>
   );
-};
-
-const styles = {
-  container: {
-    width: "100%",
-    maxWidth: "400px",
-    margin: "0 auto",
-    padding: "20px",
-  },
-  title: {
-    textAlign: "center",
-    marginBottom: "20px",
-  },
-  form: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  input: {
-    margin: "10px 0",
-    padding: "10px",
-    fontSize: "16px",
-    borderRadius: "5px",
-    border: "1px solid #ccc",
-  },
-  button: {
-    padding: "10px",
-    fontSize: "16px",
-    borderRadius: "5px",
-    backgroundColor: "#4CAF50",
-    color: "white",
-    border: "none",
-    cursor: "pointer",
-  },
-  githubButton: {
-    padding: "10px",
-    fontSize: "16px",
-    borderRadius: "5px",
-    backgroundColor: "#333",
-    color: "white",
-    border: "none",
-    cursor: "pointer",
-    marginTop: "10px",
-  },
-  error: {
-    color: "red",
-    textAlign: "center",
-    marginBottom: "10px",
-  },
 };
 
 export default LoginForm;
